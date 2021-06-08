414b7fd62374f620c2512da9157c9624868f6889
Diego Cardoso <diegocar448@hotmail.com>
2021-06-08 15:58:25 -0300

    Hibernate/JPA EclipseLink/JPA

--- src/main/java/part2/ExecutionPart2.java
+++ src/main/java/part2/ExecutionPart2.java
@@ -22,36 +22,42 @@ public class ExecutionPart2 {
         // 2.1 - Criar instancias para serem adicionadas no banco de dados
         Estado estadoParaAdicionar = new Estado("Rio de Janeiro", "RJ");
         Aluno alunoParaAdicionar = new Aluno("Daniel", 29, estadoParaAdicionar);
+        //Aluno alunoParaAdicionar2 = new Aluno("Maria", 19, estadoParaAdicionar);
+
 
         // 2.2 - Iniciar uma trasacao para adiconar as instancias no banco de dados
         entityManager.getTransaction().begin();
 
         entityManager.persist(estadoParaAdicionar);
         entityManager.persist(alunoParaAdicionar);
+        //entityManager.persist(alunoParaAdicionar2);
+
 
         entityManager.getTransaction().commit();
 
         // 3 - Resgatar instâncias no banco de dados
-//        Estado estadoEncontrado = entityManager.find(Estado.class, 1);
-//        Aluno alunoEncontrado = entityManager.find(Aluno.class, 1);
+        Estado estadoEncontrado = entityManager.find(Estado.class, 1);
+        Aluno alunoEncontrado = entityManager.find(Aluno.class, 1);
 //
-//        System.out.println(estadoEncontrado);
-//        System.out.println(alunoEncontrado);
+        System.out.println(estadoEncontrado);
+        System.out.println("Antes de mudar " + alunoEncontrado);
 
         // 4 - Alterar uma entidade
-//        entityManager.getTransaction().begin();
-//
-//        alunoEncontrado.setNome("Karam");
-//        alunoEncontrado.setIdade(20);
-//
-//        entityManager.getTransaction().commit();
+        /*entityManager.getTransaction().begin();
+
+        alunoEncontrado.setNome("Karam");
+        alunoEncontrado.setIdade(20);
+
+        entityManager.getTransaction().commit();
+
+        System.out.println("Depois de mudar " + alunoEncontrado);*/
 
         // 5 - Remover uma entidade
-//        entityManager.getTransaction().begin();
-//
-//        entityManager.remove(alunoEncontrado);
-//
-//        entityManager.getTransaction().commit();
+        entityManager.getTransaction().begin();
+
+        entityManager.remove(alunoEncontrado);
+
+        entityManager.getTransaction().commit();
 
         // 6 - Encerrar o gerenciador de entidades e encerrar a fabrica de gerenciadores de entidade.
         entityManager.close();
